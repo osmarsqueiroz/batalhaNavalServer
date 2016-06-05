e694e71b6d21a5abedf3194a5abc13f9c0ffa6bc
Osmar Queiroz <osmar@buscacliente.com.br>
2016-06-04 23:27:00 -0300

    Validando Armas e Destruição das armas

--- test/test.js
+++ test/test.js
@@ -1,42 +1,69 @@
 "use strict";
 var assert = require('assert');
-var arma = require('../lib/interface/arma');
-var armas = require('../lib/armas');
+var Arma = require('../lib/interface/Arma');
+var Couracado = require('../lib/armas/Couracado');
+var Cruzador = require('../lib/armas/Cruzador');
+var Hidroaviao = require('../lib/armas/Hidroaviao');
+var PortaAvioes = require('../lib/armas/PortaAvioes');
+var Submarino = require('../lib/armas/Submarino');
 
 describe("Validando estrutura das Classes base", function() {
 	describe("Classe arma", function() {
-		var lancha = new arma('lancha',1);
+		var lancha = new Arma('lancha',1);
 
 		it('Arma lancha', function() {
 			 //console.log(navio);
-       assert.equal(1,lancha.quadros);
+       assert.equal(1,lancha.partesRestante());
        assert.equal('lancha',lancha.nome);
 		});
 
 		it('Arma Submarino',function(){
-			let submarino = new armas.Submarino();
-			assert.equal(1,submarino.quadros);
+			let submarino = new Submarino();
+			assert.equal(1,submarino.partesRestante());
 			assert.equal('Submarino',submarino.nome);
 		});
 		it('Arma Cruzador',function(){
-			let cruzador = new armas.Cruzador();
-			assert.equal(2,cruzador.quadros);
+			let cruzador = new Cruzador();
+			assert.equal(2,cruzador.partesRestante());
 			assert.equal('Cruzador',cruzador.nome);
 		});
 		it('Arma Hidroaviao',function(){
-			let hidroaviao = new armas.Hidroaviao();
-			assert.equal(3,hidroaviao.quadros);
+			let hidroaviao = new Hidroaviao();
+			assert.equal(3,hidroaviao.partesRestante());
 			assert.equal('HidroAvião',hidroaviao.nome);
 		});
 		it('Arma Couracado',function(){
-			let couracado = new armas.Couracado();
-			assert.equal(4,couracado.quadros);
+			let couracado = new Couracado();
+			assert.equal(4,couracado.partesRestante());
 			assert.equal('Couraçado',couracado.nome);
 		});
 		it('Arma PortaAvioes',function(){
-			let portaAvioes = new armas.PortaAvioes();
-			assert.equal(5,portaAvioes.quadros);
+			let portaAvioes = new PortaAvioes();
+			assert.equal(5,portaAvioes.partesRestante());
 			assert.equal('Porta Aviões',portaAvioes.nome);
+		});
+	});
+	describe("Testar Destruição de um submarino", function() {
+		let submarino = new Submarino();
+		submarino.destruirUmaParte();
+		it("Submarino Afundado",function(){
+				assert.equal(true,submarino.verificarDestruicao());
+		});
+	});
+	describe("Testar Destruição de um portaAvioes", function() {
+
+		let portaAvioes = new PortaAvioes();
+		portaAvioes.destruirUmaParte();
+
+		it("portaAvioes não Afundado",function(){
+				assert.equal(false,portaAvioes.verificarDestruicao());
+		});
+		it("portaAvioes Afundado",function(){
+			portaAvioes.destruirUmaParte();
+			portaAvioes.destruirUmaParte();
+			portaAvioes.destruirUmaParte();
+			portaAvioes.destruirUmaParte();
+				assert.equal(0,portaAvioes.partesRestante());
 		});
 	});
 });
